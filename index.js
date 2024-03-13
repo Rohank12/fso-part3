81dd5f890c4f69e81319ddb2338e469350c995c7
Rohank12 <rohan_kelley@uri.edu>
2024-03-13 16:23:21 -0400

    production build

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
 //app.use(morgan('tiny'))
 morgan.token('post', (req, res) => {
     if (req.method === 'POST') {
@@ -93,7 +96,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
