67df74372fea244b110f5d69839340fbe54748d7
Rohank12 <rohan_kelley@uri.edu>
2024-03-15 15:47:33 -0400

    uibuild

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express()
 
 
 app.use(express.static('dist'))
-app.use(express.json())
-//app.use(cors())
 //app.use(morgan('tiny'))
 morgan.token('post', (req, res) => {
     if (req.method === 'POST') {
@@ -23,20 +21,22 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
   
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
   
     next(error)
-  }
-  
-  // this has to be the last loaded middleware, also all the routes should be registered before this!
-  app.use(errorHandler)
+}
 
-app.get('/api/persons/', (request, response) => {
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/persons/', (request, response, next) => {
     // get full list
     Person.find({}).then(persons => {
         response.json(persons)
-    })
+    }).catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -50,7 +50,7 @@ app.get('/api/persons/:id', (request, response, next) => {
       }).catch(error => next(error))
 })
 
-app.get('/api/info', (request, response) => {
+app.get('/api/info', (request, response, next) => {
     Person.countDocuments({}).then(count => {
         //console.log(count)
         response.send(`<div>
@@ -60,7 +60,7 @@ app.get('/api/info', (request, response) => {
     }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     // delete person
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
@@ -69,15 +69,8 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    // error handling
-
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({
-        error: 'information missing'
-        })
-    }
     
     const person = new Person ({
         name: body.name,
@@ -86,7 +79,7 @@ app.post('/api/persons', (request, response) => {
     
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -97,10 +90,13 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators:true }).then(updatedPerson => {
         response.json(updatedPerson)
     }).catch(error => next(error))
 })
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
